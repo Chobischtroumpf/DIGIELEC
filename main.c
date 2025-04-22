020b5aefdb11726a33f9d655346128f3629aca0f
manongortz <93400293+manongortz@users.noreply.github.com>
2025-04-22 04:02:19 +0200

    add photoresistor

--- main.c
+++ main.c
@@ -15,6 +15,8 @@
 #include "keypad.h"
 #include "semaphore.h"
 
+#define LIGHT_THRESHOLD 0.3  // Low light bar (0 to 1)
+
 
 /*
 * Set the position of the given servo.
@@ -39,6 +41,18 @@ void msg_to_semaphore(char *message){
         uint32_t l_cmp_val = sem_pos_to_cmp(l_sem);
         set_servo_pos(l_cmp_val, 1);    // 1 = left servo
         CyDelay(500);
+    }
+}
+
+
+int check_light(uint32_t *val_adc_phot) {
+    uint32_t norm_val_phot = (*val_adc_phot /(float)0xFFFF);
+    if (norm_val_phot > LIGHT_THRESHOLD) {
+        // Light is on
+        return 0;
+    } else {
+        // Light is off
+        return 1;
     }
 }
 
@@ -93,7 +107,7 @@ char *handle_keypad_press() {
     return NULL; // Return NULL if no valid key is pressed
 }
 
-void handle_UART_receive() {
+void handle_UART_receive(uint32_t *val_adc_phot) {
     char message[9] = {0};
     int index = 0;
     while (UART_1_GetRxBufferSize() != 0 && index <8)
@@ -110,30 +124,31 @@ void handle_UART_receive() {
     // we send the message via DAC (sound)
     
     // then, we check if the light is on or off
-    //if (PHOTORESISTOR_Read() == 0 && strlen(message) != 0) {
+    if (check_light(val_adc_phot) == 0) {
         //If the light is on, we send the message via semaphore (PWM)
-    //    msg_to_semaphore(message);
-    //}
+        msg_to_semaphore(message);
+    }
     // else {
         // otherwise we send it via LEDs
     // }
 }
 
 
-/*
-* Init both servos and the potentiometre.
-*/
-void init_servo_and_pot(){
-    uint16_t pwm_period = 48000;
-    PWM_DROITE_Start();
-    timer_clock_1_Start();
-    PWM_GAUCHE_Start();
-    timer_clock_2_Start();
-    PWM_DROITE_WritePeriod(pwm_period);
-    PWM_GAUCHE_WritePeriod(pwm_period);
-    ADC_POTENTIOMETRE_Start();
+void get_adc_val(uint32_t* val_adc_pot, uint32_t* val_adc_phot){
+    Mux_Select(0);
+    CyDelay(10);
     ADC_POTENTIOMETRE_StartConvert();
+    if (ADC_POTENTIOMETRE_IsEndConversion(ADC_POTENTIOMETRE_WAIT_FOR_RESULT)){
+        *val_adc_pot = ADC_POTENTIOMETRE_GetResult32();
+    }
+    Mux_Select(1);
+    CyDelay(10);
+    ADC_POTENTIOMETRE_StartConvert();
+    if(ADC_POTENTIOMETRE_IsEndConversion(ADC_POTENTIOMETRE_WAIT_FOR_RESULT)){
+        *val_adc_phot = ADC_POTENTIOMETRE_GetResult32();
+    }
 }
+
 
 
 int main(void)
@@ -142,9 +157,20 @@ int main(void)
     UART_1_Start();
     LCD_Char_1_Start();
     keypadInit();
-    init_servo_and_pot();
+    uint16_t pwm_period = 48000;
+    PWM_DROITE_Start();
+    timer_clock_1_Start();
+    PWM_GAUCHE_Start();
+    timer_clock_2_Start();
+    PWM_DROITE_WritePeriod(pwm_period);
+    PWM_GAUCHE_WritePeriod(pwm_period);
+    Mux_Start();
+    ADC_POTENTIOMETRE_Start();
     uint8_t current_servo = 0;
-    uint32_t val_adc = 0;
+    uint32_t val_adc_pot = 0;
+    uint32_t val_adc_phot = 0;
+    get_adc_val(&val_adc_pot, &val_adc_phot);
+    
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
 
@@ -161,38 +187,39 @@ int main(void)
             LCD_Char_1_PrintString(message); // Print the message on the LCD
 
             // then, we check if the light is on or off
-            //if (PHOTORESISTOR_Read() == 0) {
-            //    msg_to_semaphore(message);
-            //}
+            if (check_light(&val_adc_phot) == 0) {
                 // If the light is on, we send the message via DAC (sound)
                 //and via semaphore (PWM)
+                msg_to_semaphore(message);
+            }
+                
             // } else {
                 // If the light is off, we send the message via DAC (sound)
                 // and via LEDs
             // }
         }
 
-        handle_UART_receive();
+        handle_UART_receive(&val_adc_phot);
         
         // then we handle the switches:
         if (SWITCH_1_Read() == 0) {
             // If the switch is pressed, we send a bip via DAC as long as the switch is pressed (sound)
 
-            //if (PHOTORESISTOR_Read() != 0) {
+            //if (check_light(&val_adc_phot) != 0) {
                 // If the light is off, we send a bip via LED
           //  }
         }
         if (SWITCH_2_Read() == 0) {
             // If the switch is pressed, we send a 250ms bip via DAC (sound)
 
-            //if (PHOTORESISTOR_Read() != 0) {
+            //if (check_light(&val_adc_phot) != 0) {
                 // If the light is off, we send a 250ms bip via LED
             //}
         }
         if (SWITCH_3_Read() == 0) {
             // If the switch is pressed, we send a 750ms bip via DAC (sound)
 
-            //if (PHOTORESISTOR_Read() != 0) {
+            //if (check_light(&val_adc_phot) != 0) {
                 // If the light is off, we send a 750ms bip via LED
             //}
         }
@@ -201,15 +228,11 @@ int main(void)
             current_servo = 1 - current_servo;
             CyDelay(200);
         }
-        //if (PHOTORESISTOR_Read() == 0) {
+        if (check_light(&val_adc_phot) == 0) {
             // read the pototiometer value and use it to position the current servo
-            if (ADC_POTENTIOMETRE_IsEndConversion(ADC_POTENTIOMETRE_RETURN_STATUS)){
-                    val_adc = ADC_POTENTIOMETRE_GetResult32();
-            }
-            // (0*4)+1*1200=1200; (1*4)+1*1200=6000
-            uint32_t val_cmp = (((val_adc /(float)0xFFFF)*4) + 1 ) * 1200;
+            uint32_t val_cmp = (((val_adc_pot /(float)0xFFFF)*4) + 1 ) * 1200;      // (0*4)+1*1200=1200; (1*4)+1*1200=6000
             set_servo_pos(val_cmp, current_servo);
-        //}
+        }
         CyDelay(500);
     }
 }
